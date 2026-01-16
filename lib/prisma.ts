12681cff6b2aa0ead13657d344dd175e6a1de9cf
zetraak2 <zetraak2@gmail.com>
2026-01-16 12:48:24 +0100

    fix: force replace localhost with 127.0.0.1 in prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,11 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
     log: ["error", "warn"],
+    datasources: {
+        db: {
+            url: (process.env.DATABASE_URL || "").replace("localhost", "127.0.0.1"),
+        },
+    },
 });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

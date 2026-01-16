767762ba7174dc0ebfe753d6921241d55603800e
zetraak2 <zetraak2@gmail.com>
2026-01-16 12:51:24 +0100

    fix: make prisma url replacement safer to prevent 503 crash

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,13 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
+const prismaClientOptions: any = {
     log: ["error", "warn"],
-    datasources: {
+};
+
+if (process.env.DATABASE_URL) {
+    prismaClientOptions.datasources = {
         db: {
-            url: (process.env.DATABASE_URL || "").replace("localhost", "127.0.0.1"),
+            url: process.env.DATABASE_URL.replace("localhost", "127.0.0.1"),
         },
-    },
-});
+    };
+}
+
+export const prisma = globalForPrisma.prisma || new PrismaClient(prismaClientOptions);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
